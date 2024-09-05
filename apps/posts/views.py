ff9ccf71faed06c61f3b7d2bdc79d219e2824061
miky-rola <mikyrola8@gmail.com>
2024-09-05 08:29:00 +0000

    added pagination to post

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics
+
 from .models import Posts
 from .serializers import PostsSerializer
+from ..common.pagination import DefaultPagination
 
 class PostsListCreateView(generics.ListCreateAPIView):
     queryset = Posts.objects.all().order_by("-pub_date")
     serializer_class = PostsSerializer
+    pagination_class = DefaultPagination
 
 class PostsDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Posts.objects.all()
